b2385c26aa334cbb82be62e1c2758d5873ae7ad5
unknown <706710546@qq.com>
2022-10-07 19:11:49 +0800

    v7

--- ServerV0.7-超时强踢功能/user.go
+++ ServerV0.7-超时强踢功能/user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -71,6 +74,24 @@ func (this *User) DoMessage(msg string) {
 			this.SendMsg(onlineMsg)
 		}
 		this.server.mapLock.Unlock()
+
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式: rename|张三
+		newName := strings.Split(msg, "|")[1]
+
+		//判断name是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("您已经更新用户名:" + this.Name + "\n")
+		}
 
 	} else {
 		this.server.BroadCast(this, msg)
